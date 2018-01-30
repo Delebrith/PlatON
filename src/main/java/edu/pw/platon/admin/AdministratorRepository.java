16ac78d8c75ae5144e79afd635cee131c491b366
SaxMan96 <matidorobek@wp.pl>
2018-01-30 13:01:17 +0100

    fix conflicts

--- src/main/java/edu/pw/platon/admin/AdministratorRepository.java
+++ src/main/java/edu/pw/platon/admin/AdministratorRepository.java
@@ -1,6 +1,12 @@
 package edu.pw.platon.admin;
 
+import edu.pw.platon.user.Role;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface AdministratorRepository extends CrudRepository<Administrator, String> {
+    List<Administrator> findAllByRoles(Collection<Role> roles);
+
 }
--- src/main/java/edu/pw/platon/authority/AuthorityRepository.java
+++ src/main/java/edu/pw/platon/authority/AuthorityRepository.java
@@ -1,6 +1,11 @@
 package edu.pw.platon.authority;
 
+import edu.pw.platon.user.Role;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface AuthorityRepository extends CrudRepository<Authority, String> {
+    List<Authority> findAllByRoles(Collection<Role> roles);
 }
